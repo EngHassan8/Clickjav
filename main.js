595989a077b73c79dd9187e79da6e064dad6a687
EngHassan8 <hassannaana99@gmail>
2025-01-04 09:57:10 -0800

    click

---
+++ main.js
@@ -0,0 +1,20 @@
+let btnOpen = document.querySelector("#btnMenu")
+let btnClos = document.querySelector("#btnClose")
+let links = document.querySelector("#links")
+
+
+btnOpen.addEventListener("click", function(){
+
+    links.style.display = "flex"
+    btnOpen.style.display = "none"
+    btnClos.style.display = "flex"
+    
+})
+
+
+btnClos.addEventListener("click", function(){
+    links.style.display = "none"
+      btnOpen.style.display = "flex"
+    btnClos.style.display = "none"
+
+})
